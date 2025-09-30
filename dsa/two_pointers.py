8ddd0707bdb09c00ac0f2283448c484d263701ea
Douglas Rocha <douglasedurocha@gmail.com>
2025-09-30 19:56:33 -0300

    Refactor variable names in two_pointers function for clarity and update target value in test case

--- dsa/two_pointers.py
+++ dsa/two_pointers.py
@@ -1,20 +1,20 @@
 # Consider the arr is sorted
 def two_pointers(arr, target):
-    left, right = 0, len(arr) - 1
+    lo, hi = 0, len(arr) - 1
 
-    while left < right:
-        result = arr[left] + arr[right]
+    while lo < hi:
+        result = arr[lo] + arr[hi]
 
         if (result == target):
             return True
         if (result < target):
-            left += 1
+            lo += 1
         else:
-            right -= 1
+            hi -= 1
 
     return False
 
 
 arr = [3, 4, 5, 6]
-t = 5
+t = 9
 print(two_pointers(arr, t))
